524792568a7f4454121d53288dad2f50126be0cf
nithish38 <nnithishkumar112@gmail.com>
2025-09-16 14:58:07 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,18 @@
+L=[]
+n=int(input("Enter the number of elements:"))
+for i in range(0,n):
+    a=int(input("Enter the element:"))
+    L.append(a)
+    print(L)
+    print(L[0])
+    print(L[2:5])
+    
+
+
+
+
+
+
+
+
+
